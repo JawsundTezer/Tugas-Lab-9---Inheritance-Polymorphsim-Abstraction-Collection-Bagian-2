3b2f7e3f88ea4f909deff878a24e743a3db765c3
Jawsund Tezer <61916095+JawsundTezer@users.noreply.github.com>
2020-06-30 17:36:15 +0700

    Add files via upload

---
+++ tugaslab9/KaryawanTetap.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tugaslab9
+{
+    class KaryawanTetap : Karyawan
+    {
+        public double GajiBulanan { get; set; }
+        public override double Gaji()
+        {
+            return GajiBulanan;
+        }
+    }
+}
